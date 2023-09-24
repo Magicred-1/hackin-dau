79a4b87e948cbd3b1b2a7d941cf3d27bbb5fe939
teyik0 <teyik0.fr@icloud.com>
2023-09-24 10:38:51 +0200

    add number of nft

--- src/components/Balances.tsx
+++ src/components/Balances.tsx
@@ -1,6 +1,31 @@
+'use client';
+
+import { helloSunchineAbi } from '@/utils/helloSunChain.abi';
+import { useState } from 'react';
 import { MdOutlineAccountBalance } from 'react-icons/md';
+import { useContractRead } from 'wagmi';
+import { useAccount } from 'wagmi';
 
 const Balances = () => {
+  const [standardNFTNumber, setStandardNFTNumber] = useState<number>(0);
+  const { address, isConnecting, isDisconnected } = useAccount();
+  const { data, refetch } = useContractRead({
+    address: process.env.NEXT_PUBLIC_NFT1_CONTRACT_ADDRESS! as `0x${string}`,
+    abi: helloSunchineAbi,
+    functionName: 'getUserTokenURIs',
+    args: [address!],
+    onSuccess: (data) => {
+      console.log('SUCCESS');
+      setStandardNFTNumber(data.length);
+    },
+    onError: (error) => {
+      console.log('testerror');
+      console.log(error);
+      setTimeout(() => {
+        refetch();
+      }, 600);
+    },
+  });
   return (
     <div className='lg:col-span-1 col-span-2 bg-white rounded-xl p-6 max-h-[350px]'>
       <div className='flex gap-2'>
@@ -9,7 +34,7 @@ const Balances = () => {
       </div>
       <div className='mt-6'>
         <div>
-          <span className='text-2xl font-bold'>0</span>
+          <span className='text-2xl font-bold'>{standardNFTNumber}</span>
           <h2 className='text-xl font-bold text-slate-400'>Standard HSC</h2>
         </div>
         <div>
